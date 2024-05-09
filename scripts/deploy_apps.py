9a4830dccfc6abe83e50e44e79e4b6bf57f4c76a
Tristan Kornacki <tkornacki@rapidsos.com>
2024-05-09 11:03:18 -0400

    Updated deploy_apps

--- scripts/deploy_apps.py
+++ scripts/deploy_apps.py
@@ -1,26 +1,43 @@
 from get_apps import get_apps
-import sys
 import argparse
 
+VALID_ENVS: list[str] = ["ci", "qa-mco"]
 
-def deploy_apps(apps: list[str]):
+
+def deploy_apps():
+    apps = get_apps()
+
+    deployed_apps = []
     for app in apps:
         print(f"Deploying {app}...")
-        # deploy app
-        print(f"{app} deployed.")
-    print("All apps deployed.")
+        try:
+            deployed_apps.append(app)
+        except Exception as e:
+            print(f"Failed to deploy {app}: {e}")
+            # TODO: Do we want to continue if any fail?
+
+    for deployed_app in deployed_apps:
+        print(f"Deployed {deployed_app}")
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--environment", help="Environment to deploy to", required=True)
-    
+    parser.add_argument(
+        "-e", "--environment", help="Environment to deploy to", required=True
+    )
+
     args = parser.parse_args()
 
-    print(f'Deploying apps to {args.environment} environment')
-          
+    if args.environment not in VALID_ENVS:
+        print(
+            f"Invalid environment: {args.environment}"
+            f"Valid environments are: {VALID_ENVS}"
+        )
+        exit(1)
+
+    print(f"Deploying apps to {args.environment} environment")
+
 
 if __name__ == "__main__":
-    # Get the cli args
-    args = parse_args()
-    apps = get_apps()
-    deploy_apps(apps)
+    parse_args()
+    deploy_apps()
