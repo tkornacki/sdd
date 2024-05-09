aba64e9309c524a7056fa12cb6f1a0ed10e52533
Tristan Kornacki <tkornacki@rapidsos.com>
2024-05-09 10:56:54 -0400

    updated deploy

--- scripts/deploy_apps.py
+++ scripts/deploy_apps.py
@@ -1,4 +1,6 @@
 from get_apps import get_apps
+import sys
+import argparse
 
 
 def deploy_apps(apps: list[str]):
@@ -8,7 +10,17 @@ def deploy_apps(apps: list[str]):
         print(f"{app} deployed.")
     print("All apps deployed.")
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--environment", help="Environment to deploy to", required=True)
+    
+    args = parser.parse_args()
+
+    print(f'Deploying apps to {args.environment} environment')
+          
 
 if __name__ == "__main__":
+    # Get the cli args
+    args = parse_args()
     apps = get_apps()
     deploy_apps(apps)
